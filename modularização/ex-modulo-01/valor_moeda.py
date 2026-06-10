4ae011c25c879bd42027103bd16c8bbdcb9f54c9
Samuel Vaz <svaz.samuel@gmail.com>
2026-06-10 13:58:31 -0300

    Delete modularização directory

--- modularização/ex-modulo-01/valor_moeda.py
+++
@@ -1,7 +0,0 @@
-from ex107 import moeda
-
-p = int(input('Digite o valor: R$'))
-print(f'A metade de {p} é {moeda.metade(p)}')
-print(f'O dobro de {p} é {moeda.dobro(p)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(p, 10)}')
-print(f'Diminuindo 13% temos {moeda.diminuir(p, 13)}')
